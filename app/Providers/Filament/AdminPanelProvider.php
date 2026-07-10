42de6c3c30b42261ed1e4665f627ccd8571be581
Jose Dalanon <josedalanon630@gmail.com>
2026-07-10 20:35:32 +0800

    just testing the action

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,7 +54,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->sidebarCollapsibleOnDesktop()
             ->colors([
-                'primary' => Color::Red, // must be controllable by user
+                'primary' => Color::Amber, // must be controllable by user
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
